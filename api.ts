818c23aacada84fc88c77bdeab69d810980cbd91
nemusona <nemusonaneko@protonmail.com>
2023-06-05 15:53:18 -0500

    autoremoval

--- api.ts
+++ api.ts
@@ -192,6 +192,14 @@ app.post(
 
       const job = await queue.add(model, payload, {
         delay: delay,
+        removeOnComplete: {
+          age: 300,
+          count: 100
+        },
+        removeOnFail: {
+          age: 300,
+          count: 100
+        }
       });
       return res.status(201).send(job.id);
     } catch (error: any) {
